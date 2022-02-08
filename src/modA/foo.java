eef3b59ec867b47a983c74230962075b3bca9a08
Daniel Kerbel <nmdanny@gmail.com>
2022-02-08 03:56:58 +0200

    modify two places(two hunks)

--- src/modA/foo.java
+++ src/modA/foo.java
@@ -3,12 +3,12 @@ package modA;
 
 class Foo {
     void fooFunc() {
-        System.out.println("fooFunc");
+        System.out.println("fooFunc method body changed");
     }
 }
 
 class Baz {
-    void bazFunc() {
-        System.out.println("bazFunc");
+    void bazFunc(int k) {
+        System.out.println("bazFunc declaration and body changed");
     }
 }
