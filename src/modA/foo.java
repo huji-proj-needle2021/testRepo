a09a5c940e6c64917321a56fa3122c0778fa9a16
Daniel Kerbel <nmdanny@gmail.com>
2022-02-08 03:48:21 +0200

    add two classes

---
+++ src/modA/foo.java
@@ -0,0 +1,14 @@
+package modA;
+
+
+class Foo {
+    void fooFunc() {
+        System.out.println("fooFunc");
+    }
+}
+
+class Baz {
+    void bazFunc() {
+        System.out.println("bazFunc");
+    }
+}
